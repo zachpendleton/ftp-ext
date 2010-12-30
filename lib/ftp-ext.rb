ebb44fa3ae838789f5d7551b162d3d9151d0a667
Zach Pendleton <zachpendleton@gmail.com>
2010-12-29 17:48:06 -0700

    It actually works now (surprise!).
    Made sync_dir protected and now call it from put_dir when remote
    directory already exists. Recursion is smarter now, too. Rewrote
    README with examples, and swapped it to Markdown. Added a basic
    test.

--- lib/ftp-ext.rb
+++ lib/ftp-ext.rb
@@ -1,52 +1,68 @@
-%w{rubygems net/ftp ptools}.each do |lib|
+%w(rubygems net/ftp ptools).each do |lib|
   require lib
 end
 
 module Net
   class FTP
-    
-    def put_dir(local_folder, remote_folder = '.')
-      rmdir(File.join(remote_folder, local_folder)) if file_exists? File.join(remote_folder, local_folder)
+    def put_dir(local, remote = ".", erase = false)
+      dirname = File.basename(local)
+      current_local  = Dir.pwd
+      current_remote = pwd
       
-      Dir.chdir(local_folder)
-      chdir(remote_folder)
-
-      mkdir(File.basename(local_folder))
-      chdir(File.basename(local_folder))
-
-      Dir.foreach('.') do |f|      
+      # Change directory
+      Dir.chdir(local)
+      chdir(remote)
+      
+      # Make new directory or, if it exists, begin sync
+      begin
+        mkdir(dirname)
+      rescue Exception => e
+        Dir.chdir(current_local)
+        chdir(current_remote)
+        sync_dir(local, dirname)
+        return
+      end
+      chdir(dirname)
+      
+      # Upload files recursively
+      Dir.foreach(".") do |f|
         if File.file?(f)
           File.binary?(f) ? putbinaryfile(f) : puttextfile(f)
         elsif !f.match(/^\.+$/)
-          put_dir(File.path(f))
-        end
-      end
-
-      Dir.chdir('..')
-      chdir('..')
-    end
-
-    def sync_dir(local_folder, remote_folder = '.')
-      mkdir(remote_folder) if !file_exists?(remote_folder)
-      
-      Dir.chdir(local_folder)
-      chdir(remote_folder)
-      
-      Dir.foreach('.') do |f|
-        if File.file?(f)
-          if !file_exists?(f) || mtime(f) < File.mtime(f)
-            File.binary?(f) ? putbinaryfile(f) : puttextfile(f)
-          end
-        elsif !f.match(/^\.+$/)
-          mkdir(File.basename(f)) if !file_exists?(f)
-          sync_dir(File.path(f), f)
+          put_dir(f, pwd)
         end
       end
       
+      # Clean up
       Dir.chdir('..')
       chdir('..')
     end
     
+    def sync_dir(local, remote = ".")
+      # Change directory
+      Dir.chdir(local)
+      chdir(remote)
+      
+      # Loop through directory and compare/update files
+      Dir.foreach(".") do |f|
+        if File.file?(f)
+          # Add new files and replace old files
+          if !file_exists?(f) || mtime(f) < File.mtime(f)
+            File.binary?(f) ? putbinaryfile(f) : puttextfile(f)
+          end
+        # Recurse if the file is a directory
+        elsif !f.match(/^\.+$/)
+          mkdir(File.basename(f)) if !file_exists?(f)
+          sync_dir(f, File.join(pwd, f))
+        end
+      end
+      
+      # Clean up
+      Dir.chdir("..")
+      chdir("..")
+
+    end
+
     protected
     def file_exists?(file)
       list = nlst
--- test/test_ftp-ext.rb
+++ test/test_ftp-ext.rb
@@ -1,5 +1,19 @@
 require 'helper'
 
-class TestFtpExt < Test::Unit::TestCase
-  
+class FTPExtTest < Test::Unit::TestCase
+  context "FTPExt should" do
+
+    setup do
+      @src = 'htdocs'
+    end
+
+    should "upload a directory to a linux machine" do
+      ftp = Net::FTP.new('localhost')
+      ftp.login('testuser', 'testpass')
+      
+      ftp.put_dir(@src)
+      puts ftp.nlst
+      assert true
+    end
+  end
 end
