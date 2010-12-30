3d4bb93911b1b745e63480795d8c1235a7df67df
Zach Pendleton <zachpendleton@gmail.com>
2010-12-30 13:48:25 -0700

    Added verbose, overwrite options to put_dir.

--- lib/ftp-ext.rb
+++ lib/ftp-ext.rb
@@ -4,69 +4,132 @@ end
 
 module Net
   class FTP
-    def put_dir(local, remote = ".", erase = false)
-      dirname = File.basename(local)
-      current_local  = Dir.pwd
-      current_remote = pwd
-      
-      # Change directory
-      Dir.chdir(local)
-      chdir(remote)
-      
-      # Make new directory or, if it exists, begin sync
-      begin
-        mkdir(dirname)
-      rescue Exception => e
-        Dir.chdir(current_local)
-        chdir(current_remote)
-        sync_dir(local, dirname)
+    def put_dir(opts = {})
+      # Set default options
+      options = {
+        :local     => ".",
+        :remote    => ".",
+        :verbose   => false,
+        :erase     => false
+      }.merge!(opts)
+            
+      # Check for existence of directory on remote server
+      # and create it if it isn't there. If it does exist,
+      # call sync_dir instead.
+      if !remote_file_exists?(options[:remote])
+        file_segments = options[:remote].split(File::SEPARATOR)
+        
+        # Make directory and all missing parent directories
+        file_segments.length.times do |n|
+          fpath = File.join(file_segments[0..n])
+          
+          unless remote_file_exists?(fpath) || fpath == ""
+            puts "mkdir #{fpath}" if options[:verbose] == true
+            mkdir(fpath)
+          end
+        end
+      else
+        options[:erase] ? (rmrf_dir(options[:remote], options[:verbose]); put_dir(options)) : sync_dir(options)
         return
       end
-      chdir(dirname)
       
       # Upload files recursively
-      Dir.foreach(".") do |f|
-        if File.file?(f)
-          File.binary?(f) ? putbinaryfile(f) : puttextfile(f)
+      Dir.foreach(options[:local]) do |f|
+        local  = File.join(options[:local], f)
+        remote = File.join(options[:remote], f)
+
+        if File.file?(local)
+          puts "cp #{remote}" if options[:verbose] == true
+          File.binary?(local) ? putbinaryfile(local, remote) : puttextfile(local, remote)
+          
+        # ignore . and .., but upload other directories
         elsif !f.match(/^\.+$/)
-          put_dir(f, pwd)
+          put_dir(options.merge({ :local => local, :remote => remote }))
         end
+        
       end
       
-      # Clean up
-      Dir.chdir('..')
-      chdir('..')
     end
     
-    def sync_dir(local, remote = ".")
-      # Change directory
-      Dir.chdir(local)
-      chdir(remote)
+    def rmrf_dir(dir, verbose = false)
+      return if !remote_file_exists?(dir)
       
-      # Loop through directory and compare/update files
-      Dir.foreach(".") do |f|
-        if File.file?(f)
-          # Add new files and replace old files
-          if !file_exists?(f) || mtime(f) < File.mtime(f)
-            File.binary?(f) ? putbinaryfile(f) : puttextfile(f)
+      # Get file list from the server and begin deleting files
+      begin
+        flist = nlst(dir)
+        
+        flist.each do |f|
+          # Ignore . and ..
+          next if f.match(/^\.+$/)
+          
+          # Try to delete the file. If we fail then it's a directory
+          begin
+            delete(f)
+            puts "rm #{f}" if verbose
+          rescue Exception => e
+            rmrf_dir(f, verbose)
           end
-        # Recurse if the file is a directory
-        elsif !f.match(/^\.+$/)
-          mkdir(File.basename(f)) if !file_exists?(f)
-          sync_dir(f, File.join(pwd, f))
+          
         end
+      rescue Exception => e
+        # If the directory is empty, error silently and continue execution
       end
       
-      # Clean up
-      Dir.chdir("..")
-      chdir("..")
-
+      rmdir(dir)
+      puts "rm -rf #{dir}" if verbose
+      
     end
-
+    
     protected
-    def file_exists?(file)
-      list = nlst
-      nlst.include?(file)
+    def sync_dir(options = {})
+      # Loop through each directory and compare/update the files
+      Dir.foreach(options[:local]) do |f|
+        local  = File.join(options[:local], f)
+        remote = File.join(options[:remote], f)
+        
+        if File.file?(local)
+          if !remote_file_exists?(remote) || mtime(remote) < File.mtime(local)
+            puts "updating #{remote}" if options[:verbose] == true
+            File.binary?(local) ? putbinaryfile(local, remote) : puttextfile(local, remote)
+            
+          else
+            puts "skipping #{remote}" if options[:verbose] == true
+          end
+        elsif !f.match(/^\.+$/)
+          put_dir(options.merge(:local => local, :remote => remote))
+        end
+      
+      end
+      
+    end
+    
+    def remote_file_exists?(file)
+      fpath = file.split(File::SEPARATOR)
+
+      # If we're only one directory deep, don't try to join anything
+      if fpath.length == 1
+        # starting at file system root?
+        if file.match(/^#{File::SEPARATOR}/)
+          return nlst(File::SEPARATOR).include?(file)
+        # starting relative to FTP dir
+        else
+          return nlst.include?(file)
+        end
+      # Nested directories
+      else
+        fname = fpath.pop
+        fpath.map! { |v| v == "" ? File::SEPARATOR : v }
+        fpath = fpath.join(File::SEPARATOR)
+
+        # Wrap return in begin..rescue because nlst errors on empty directory
+        begin
+          return nlst(fpath).include?(File.join(fpath, fname))
+        rescue Exception => e
+          return false if e.message.match("450")
+        end
+
+      end
+
     end
     
   end
